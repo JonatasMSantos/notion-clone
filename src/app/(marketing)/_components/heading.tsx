6d46b07f5880158e9990a15f960311dc17407ab9
Jônatas Santos <jts.msantos@gmail.com>
2024-01-24 16:57:50 -0300

    feat() init convex setup and theme provider; add marketing components

--- src/app/(marketing)/_components/heading.tsx
+++ src/app/(marketing)/_components/heading.tsx
@@ -5,10 +5,10 @@ export const Heading = () => {
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
         Your Ideas, Documents, & Plans. Unified. Welcome to
-        <span className="underline">Jotion</span>
+        <span className="underline ml-3">JNotes</span>
       </h1>
       <h3 className="text-base sm:text-xl md:text-2xl font-medium">
-        Reporte.me Notes is the connected workspace where <br />
+        JNotes is the connected workspace where <br />
         better, faster work happens.
       </h3>
     </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,16 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Toaster } from "sonner";
+
+import { ThemeProvider } from "@/providers/theme-provider";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Reporte.me - notes",
-  description: "Reporte your notes in the best workspace.",
+  title: "JNotes",
+  description: "The connected workspace where better, faster work happens.",
   icons: {
     icon: [
       {
@@ -25,12 +29,24 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+          storageKey="JNotes-theme-2"
+        >
+          <Toaster position="bottom-center" />
+
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
